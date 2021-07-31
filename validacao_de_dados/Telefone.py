42ae9bf8fb3065331962813caae8ccdfbd624401
VictorGM01 <victormarques8801@gmail.com>
2021-07-31 13:26:26 -0300

    # validação de Telefones

---
+++ validacao_de_dados/Telefone.py
@@ -0,0 +1,33 @@
+import re
+
+class Telefones:
+
+    def __init__(self, telefone):
+        if self.valida_telefone(telefone):
+            self.numero = telefone
+        else:
+            raise ValueError("O número de telefone é inválido!")
+
+        if len(telefone) > 13:
+            raise ValueError("A quantidade de caracteres está incorreta!")
+
+    def __str__(self):
+        return self.formata_telefone()
+
+    def valida_telefone(self, telefone):
+        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        resposta = re.findall(padrao, telefone)
+        if resposta:
+            return True
+        else:
+            return False
+
+    def formata_telefone(self):
+        padrao = "([0-9]{2})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        resposta = re.search(padrao, self.numero)
+        numero_formatado = f"+{resposta.group(1)}({resposta.group(2)}){resposta.group(3)}-{resposta.group(4)}"
+        numero_formatado_sem_codigo_pais = f"({resposta.group(2)}){resposta.group(3)}-{resposta.group(4)}"
+        if resposta.group(1) == None:
+            return numero_formatado_sem_codigo_pais
+        else:
+            return numero_formatado
